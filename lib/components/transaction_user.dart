60693acaf6f857488c79c367aa013546416f3a25
Luis.souza03 <luis93667@gmail.com>
2022-08-29 21:44:18 -0300

    feat: add scroll in transaction list

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -26,6 +26,36 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 110.76,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de luz',
+      value: 110.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Conta de luz',
+      value: 110.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Conta de luz',
+      value: 110.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Conta de luz',
+      value: 110.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'Conta de luz',
+      value: 110.76,
+      date: DateTime.now(),
+    ),
   ];
 
   _addTransaction(String title, double value) {
@@ -45,8 +75,8 @@ class _TransactionUserState extends State<TransactionUser> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TransactionList(transactions: _transactions),
         TransactionForm(onSubmit: _addTransaction),
+        TransactionList(transactions: _transactions),
       ],
     );
   }
